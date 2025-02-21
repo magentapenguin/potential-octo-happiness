40c44b0f0ae4e8a00c1d19780c75279da30c6c97
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-02-21 21:04:17 +0000

    add a bunch of stuff + icons

---
+++ src/bsicons.ts
@@ -0,0 +1,38 @@
+
+export default class BootstrapIcon extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        const name = this.getAttribute('name');
+        const size = this.getAttribute('size') ?? '1em';
+
+        // load the icon from '/icons' folder
+        fetch(`/icons/${name}.svg`, {
+            cache: 'force-cache'
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to load icon ${name}`);
+                }
+                if (response.headers.get('Content-Type') !== 'image/svg+xml') {
+                    throw new Error(`Invalid content type for icon ${name} - ${response.headers.get('Content-Type')}`);
+                }
+                return response.text()
+            })
+            .then(svg => {
+                this.innerHTML = svg;
+                this.style.width = size;
+                this.style.height = size;
+                const svgElement = this.querySelector('svg');
+                if (svgElement) {
+                    svgElement.style.width = '100%';
+                    svgElement.style.height = '100%';
+                }
+            }
+        );
+    }
+}
+
+customElements.define('bs-icon', BootstrapIcon);
